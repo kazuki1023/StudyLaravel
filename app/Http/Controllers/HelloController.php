eb3b60a6c1082110debbe6180b01db6835b6be4b
kazuki1023 <k.keio12569@gmail.com>
2023-03-11 09:35:19 +0000

    p.195 パラメーター結合の利用終了

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,7 +13,12 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select("select * from people");
+        if (isset($request->id)) {
+            $param = ['id' => $request->id];
+            $items = DB::select("select * from people where id = :id" ,$param);
+        } else {
+            $items = DB::select("select * from people");
+        }
         return view("hello.index", ['items' => $items ]);
     }
 
