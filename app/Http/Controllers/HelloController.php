2610d48a3712b071e2e69c821001d7d331532eca
kazuki1023 <k.keio12569@gmail.com>
2023-03-11 09:59:28 +0000

    p199 DB::insertによるレコード作成終了

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,16 +13,24 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if (isset($request->id)) {
-            $param = ['id' => $request->id];
-            $items = DB::select("select * from people where id = :id" ,$param);
-        } else {
-            $items = DB::select("select * from people");
-        }
+        $items = DB::select("select * from people");
         return view("hello.index", ['items' => $items ]);
     }
 
     public function post(HelloRequest $request) {
         return view('hello/index', ['msg' => '正しく入力されました。']);
+    }
+
+    public function add(Request $request) {
+        return view('hello.add');
+    }
+    public function create(Request $request) {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ use App\Http\Middleware\HelloMiddleware;
 Route::get('hello', [HelloController::class, "index"]);
 Route::post('hello', [HelloController::class, "post"]);
 
+
+Route::get('hello/add', [HelloController::class, "add"]);
+Route::post('hello/add', [HelloController::class, "create"]);
+
