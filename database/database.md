c4d3f5359a632aee243927390156c662922379a9
kazuki1023 <k.keio12569@gmail.com>
2023-03-11 00:04:34 +0000

    p194 DBクラスとは終了

---
+++ database/database.md
@@ -0,0 +1,5 @@
+## memo 
+
+### DB::selectの利用
+- $変数 = DB::select(実行するsql文);
+  - DB::selectは、sqlクエリ-を実行し、結果となるレコードを取得するもの。
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,52 +7,18 @@
 @endsection
 
 @section('content')
-  <p>{{$msg}}</p>
-  @if(count($errors) > 0)
-  <p>入力にもんだいがあります。再入力してください。</p>
-  @endif
-  <form action="/hello" method="POST">
-    <table>
-    @csrf
-    @if ($errors->has('name'))
+<table>
+  <tr>
+    <th>name</th>
+    <th>mail</th>
+    <th>age</th>
+  </tr>
+  @foreach ($items as $item) 
     <tr>
-      <th>error</th>
-      <td>{{$errors->first('name')}}</td>
+      <td>{{$item ->name}}</td>
+      <td>{{$item ->mail}}</td>
+      <td>{{$item ->age}}</td>
     </tr>
-    @endif
-    <tr>
-      <th>name:</th>
-      <td><input type="text" name="name" value="{{old('name')}}"></td>
-    </tr>
-
-    @if ($errors->has('mail'))
-    <tr>
-      <th>error</th>
-      <td>{{$errors->first('mail')}}</td>
-    </tr>
-    @endif
-    <tr>
-      <th>mail:</th>
-      <td><input type="text" name="mail" value="{{old('mail')}}"></td>
-    </tr>
-
-    @if ($errors->has('age'))
-    <tr>
-      <th>error</th>
-      <td>{{$errors->first('age')}}</td>
-    </tr>
-    @endif
-    <tr>
-      <th>age:</th>
-      <td><input type="text" name="age" value="{{old('age')}}"></td>
-    </tr>
-    <tr>
-      <th></th>
-      <td><input type="submit" value="send"></td>
-    </tr>
-    </table>
-  </form>
-
-@section('footer')
-copyright 2023 kazuki.
+  @endforeach
+</table>
 @endsection
