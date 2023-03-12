fccccb7e0587b2bacfa0674820ce22493527ed76
kazuki1023 <k.keio12569@gmail.com>
2023-03-12 23:48:24 +0000

    p210 演算記号を指定した検索終了

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -67,7 +67,7 @@ class HelloController extends Controller
 
     public function show(Request $request) {
         $id = $request->id;
-        $item = DB::table('people')->where('id', $id)->first();
-        return view('hello.show', ['item' =>$item]);
+        $items = DB::table('people')->where('id', "<=" ,$id)->get();
+        return view('hello.show', ['items' => $items]);
     }
 }
--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -7,24 +7,18 @@
 @endsection
 
 @section('content')
+  @if ($items != null)
+    @foreach ($items as $item)
     <table>
       <tr>
-        <th>name:</th>
+        <th>id:</th>
         <td>{{$item->id}}</td>
-      </tr>
-      <tr>
         <th>name:</th>
         <td>{{$item->name}}</td>
       </tr>
-      <tr>
-        <th>mail:</th>
-        <td>{{$item->mail}}</td>
-      </tr>
-      <tr>
-        <th>age:</th>
-        <td>{{$item->age}}</td>
-      </tr>
     </table>
+    @endforeach
+  @endif
 @endsection
 
 @section('footer')
