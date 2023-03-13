699b235349cfab8c54cd35c285ea8bcd61ba033d
kazuki1023 <k.keio12569@gmail.com>
2023-03-13 20:30:45 +0000

    p.214 whereRawによる条件検索終了

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -66,11 +66,11 @@ class HelloController extends Controller
     }
 
     public function show(Request $request) {
-        $name = $request->name;
+        $min = $request ->min;
+        $max = $request ->max;
         $items = DB::table('people')
-        ->where('name', "like", "%" . $name . "%")
-        ->orWhere("mail", "like", "%" . $name . "%")
-        ->get();
+        ->whereRaw('age >= ? and age <= ?' ,
+        [$min, $max])->get();
         return view('hello.show', ['items' => $items]);
     }
 }
