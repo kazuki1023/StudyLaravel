6d80487f73568c7bcd55a365906e6086f9d222f7
kazuki1023 <k.keio12569@gmail.com>
2023-03-13 20:26:34 +0000

    p212 where とorwhere　終了

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -66,8 +66,11 @@ class HelloController extends Controller
     }
 
     public function show(Request $request) {
-        $id = $request->id;
-        $items = DB::table('people')->where('id', "<=" ,$id)->get();
+        $name = $request->name;
+        $items = DB::table('people')
+        ->where('name', "like", "%" . $name . "%")
+        ->orWhere("mail", "like", "%" . $name . "%")
+        ->get();
         return view('hello.show', ['items' => $items]);
     }
 }
