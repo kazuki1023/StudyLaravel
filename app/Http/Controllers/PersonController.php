c1e9d3277b95bb389a7dd3caea2afc236cd37f92
kazuki1023 <k.keio12569@gmail.com>
2023-03-15 21:16:25 +0000

    p251 スコープを組み合わせる　終了

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -19,7 +19,11 @@ class PersonController extends Controller
 
     // post送信されたときの処理
     public function search(Request $request) {
-        $item = Person::find($request -> input);
+        $min = $request ->input * 1;
+        $max = $min * 10;
+        $item = Person::ageGreaterThan($min)
+        ->ageLessThan($max)
+        ->first();
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
